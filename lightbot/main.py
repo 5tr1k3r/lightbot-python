5ce81b0c65d4d46db988ecee74da410a5b81ed3f
Alexander <noblemonitor@gmail.com>
2016-11-19 23:40:54 +0500

    feat($main): add basic camera actions

--- lightbot/main.py
+++ lightbot/main.py
@@ -1,5 +1,3 @@
-from collections import namedtuple
-
 import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
@@ -25,6 +23,31 @@ class Vertex:
         return self.x, self.y, self.z
 
 
+class Floor:
+    def __init__(self, width, height, level):
+        self.level = level
+
+        self.vertices = []
+        for x in range(width + 1):
+            for y in range(height + 1):
+                self.vertices.append(Vertex(x, y, level))
+
+        self.big_rect = (0,
+                         self.vertices.index(Vertex(0, height, level)),
+                         self.vertices.index(Vertex(width, height, level)),
+                         self.vertices.index(Vertex(width, 0, level)))
+
+        self.edges = []
+        for i in range(1, height):
+            x = self.vertices.index(Vertex(0, i, level))
+            y = self.vertices.index(Vertex(width, i, level))
+            self.edges.append((x, y))
+        for i in range(1, width):
+            x = self.vertices.index(Vertex(i, 0, level))
+            y = self.vertices.index(Vertex(i, height, level))
+            self.edges.append((x, y))
+
+
 # noinspection PyShadowingNames
 class LightbotOpenGLWindow:
     WINDOW_WIDTH = 800
@@ -34,61 +57,65 @@ class LightbotOpenGLWindow:
     width = 5
     height = 4
 
-    Floor = namedtuple('Floor', 'vertices, edges, big_rect')
-
     def __init__(self):
         pygame.init()
         display = (self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
         pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        # glEnable(GL_DEPTH_TEST)  # THIS STUFF MAKES THINGS OPAQUE/SOLID!
+        self._reset_camera_position()
+
+        self.ground = Floor(self.width, self.height, level=0)
+        self.second_floor = Floor(3, 3, level=1)
+
+        self.start_game_cycle()
+
+    def _reset_camera_position(self):
+        glLoadIdentity()
         gluPerspective(45, self.WINDOW_WIDTH / self.WINDOW_HEIGHT, 0.1, 50.0)
         glTranslatef(-2, 0, -7)
         glRotatef(-35.264, 1.0, 0.0, 0.0)
         glRotatef(-45.0, 0.0, 0.0, 1.0)
         glRotatef(5, 0, 0, 1)
         glRotatef(-4, 1, 0, 0)
-        # glEnable(GL_DEPTH_TEST) # THIS STUFF MAKES THINGS OPAQUE/SOLID!
 
-        self.ground = self._make_floor(level=0)
-        self.second_floor = self._make_floor(level=1)
+    def start_game_cycle(self):
+        while True:
+            self._process_events()
+            self._clear_screen()
 
-        self.start_game_cycle()
+            # All custom draw functions should go here.
+            self._draw_floor(self.ground, grid=True)
+            self._draw_floor(self.second_floor, grid=True,
+                             color=(0.5, 0.5, 0.5))
+            self._draw_current_state()
 
-    def _make_floor(self, level):
-        vertices = []
-        edges = []
+            self._update_screen()
 
-        for x in range(self.width + 1):
-            for y in range(self.height + 1):
-                vertices.append(Vertex(x, y, level))
-        # print(self.vertices)
+    def _process_events(self, log=None):
+        # Here we check keys that are held down
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            glRotatef(1, 1, 0, 0)
+        if keys[pygame.K_RIGHT]:
+            glRotatef(1, -1, 0, 0)
+        if keys[pygame.K_DOWN]:
+            glRotatef(1, 0, 1, 0)
+        if keys[pygame.K_UP]:
+            glRotatef(1, 0, -1, 0)
 
-        big_rect = (0,
-                    vertices.index(Vertex(0, self.height, level)),
-                    vertices.index(Vertex(self.width, self.height, level)),
-                    vertices.index(Vertex(self.width, 0, level)))
-
-        for i in range(1, self.height):
-            x = vertices.index(Vertex(0, i, level))
-            y = vertices.index(Vertex(self.width, i, level))
-            edges.append((x, y))
-
-        for i in range(1, self.width):
-            x = vertices.index(Vertex(i, 0, level))
-            y = vertices.index(Vertex(i, self.height, level))
-            edges.append((x, y))
-
-        return self.Floor(vertices, edges, big_rect)
-
-    @staticmethod
-    def _process_events(log=None):
+        # Get event queue and check it for a few specific events
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            esc_is_pressed = (event.type == pygame.KEYDOWN and
+                              event.key == pygame.K_ESCAPE)
+            home_is_pressed = (event.type == pygame.KEYDOWN and
+                               event.key == pygame.K_HOME)
+
+            if event.type == pygame.QUIT or esc_is_pressed:
                 pygame.quit()
                 quit()
 
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    glRotatef(-1, 1, 0, 0)
+            if home_is_pressed:
+                self._reset_camera_position()
 
             if log:
                 print(event)
@@ -102,38 +129,35 @@ class LightbotOpenGLWindow:
         pygame.display.flip()
         pygame.time.wait(10)
 
-    def start_game_cycle(self):
-        while True:
-            self._process_events()
-            self._clear_screen()
-
-            # All custom draw functions should go here.
-            self._draw_floor(self.ground, grid=True)
-            self._draw_floor(self.second_floor)
-            self.draw_current_state()
-
-            self._update_screen()
-
-    def draw_current_state(self):
+    def _draw_current_state(self):
         pass
 
-    @staticmethod
-    def _draw_floor(floor, grid=None):
+    def _draw_floor(self, floor, grid=None, color=None):
         # self._draw_text(0, 0, 0, 'hey')
-
-        glBegin(GL_QUADS)
-        glColor3f(0.2, 0.4, 0.4)
-        for vertex in floor.big_rect:
-            glVertex3fv(floor.vertices[vertex]())
-        glEnd()
+        if not color:
+            color = (0.2, 0.4, 0.4)
+        self._draw_rect(floor.vertices, floor.big_rect, color)
 
         if grid:
-            glBegin(GL_LINES)
-            glColor3f(0, 0, 0)
-            for edge in floor.edges:
-                for vertex in edge:
-                    glVertex3fv(floor.vertices[vertex]())
-            glEnd()
+            line_color = (0, 0, 0)
+            self._draw_lines(floor.vertices, floor.edges, line_color)
+
+    @staticmethod
+    def _draw_rect(vertices, rect, color):
+        glBegin(GL_QUADS)
+        glColor3f(*color)
+        for vertex in rect:
+            glVertex3fv(vertices[vertex]())
+        glEnd()
+
+    @staticmethod
+    def _draw_lines(vertices, edges, color):
+        glBegin(GL_LINES)
+        glColor3f(*color)
+        for edge in edges:
+            for vertex in edge:
+                glVertex3fv(vertices[vertex]())
+        glEnd()
 
     @staticmethod
     def _draw_text(x, y, z, text):
@@ -149,5 +173,5 @@ class LightbotOpenGLWindow:
 
 
 if __name__ == '__main__':
-    board = game.Board('board.txt')
+    board = game.Board('lightbot/board.txt')
     window = LightbotOpenGLWindow()
