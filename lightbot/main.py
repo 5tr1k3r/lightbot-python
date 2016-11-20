c2927b00934d01e183e1bbcd7f376124f84a1a46
Alexander <noblemonitor@gmail.com>
2016-11-20 18:53:41 +0500

    docs($main): added docstrings

--- lightbot/main.py
+++ lightbot/main.py
@@ -1,15 +1,20 @@
+"""Main module for the project."""
 import os
 import sys
 
 import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
-from pygame.locals import *
+from pygame import locals as pyloc
 
 from lightbot import game
 
+
 # noinspection PyShadowingNames
 class Vertex:
+    """Implement a Vertex class.
+    We can check equality of Vertices.
+    """
     def __init__(self, x=0, y=0, z=0):
         self.x = x
         self.y = y
@@ -23,6 +28,13 @@ class Vertex:
 
 
 class Floor:
+    """Construct a floor object of a given size (in cells) and a given elevation.
+    It is represented by:
+    - all its vertices (a list of Vertex objects)
+    - a big rectangle that covers the whole area (a tuple with 4 indices of vertices)
+    - all the inner edges, e.g. 3x2 floor has 3 those edges
+      each edge is a tuple with 2 indices of vertices
+    """
     def __init__(self, width, height, level):
         self.level = level
 
@@ -49,6 +61,7 @@ class Floor:
 
 # noinspection PyShadowingNames
 class LightbotOpenGLWindow:
+    """A basic pygame-opengl window class."""
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
     DESIRED_FPS = 100
@@ -58,7 +71,7 @@ class LightbotOpenGLWindow:
 
     def __init__(self):
         self.init_window()
-        self.reset_camera_position()
+        self.set_default_camera_position()
 
         self.ground = Floor(self.width, self.height, level=0)
         self.second_floor = Floor(3, 3, level=1)
@@ -66,27 +79,32 @@ class LightbotOpenGLWindow:
         self.start_game_cycle()
 
     def init_window(self):
+        """Initialization and first few important OpenGL functions."""
         if sys.platform in ["win32", "win64"]:
             os.environ["SDL_VIDEO_CENTERED"] = "1"
         pygame.init()
         display = (self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
-        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        pygame.display.set_mode(display, pyloc.DOUBLEBUF | pyloc.OPENGL)
         # THIS STUFF MAKES THINGS OPAQUE/SOLID!
         glEnable(GL_DEPTH_TEST)
         glDepthFunc(GL_LEQUAL)
-        glEnable(GL_MULTISAMPLE)
+        # glEnable(GL_MULTISAMPLE)
         # glEnable(GL_LINE_SMOOTH)
 
-    def reset_camera_position(self):
+    def set_default_camera_position(self):
+        """Trying to make it look like original lightbot's isometric perspective."""
         glLoadIdentity()
         gluPerspective(45, self.WINDOW_WIDTH / self.WINDOW_HEIGHT, 0.1, 50.0)
         glTranslatef(-2, 0, -7)
-        glRotatef(-35.264, 1.0, 0.0, 0.0)
-        glRotatef(-45.0, 0.0, 0.0, 1.0)
+        glRotatef(-35.264, 1, 0, 0)
+        glRotatef(-45.0, 0, 0, 1)
         glRotatef(5, 0, 0, 1)
         glRotatef(-4, 1, 0, 0)
 
     def start_game_cycle(self):
+        """Quite self-explanatory. Process events, prepare for drawing, do drawing.
+        Should add game logic in there somewhere.
+        """
         while True:
             self._process_events()
             self._clear_screen()
@@ -100,21 +118,24 @@ class LightbotOpenGLWindow:
             self._update_screen()
 
     def _process_events(self, log=None):
-        # Here we check keys that are held down
+        """Events are keyboard and mouse button presses, mouse movement
+        and other stuff like resizing the window.
+        """
+        # Here we check which keys are held down
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            glRotatef(1, 1, 0, 0)
-        if keys[pygame.K_RIGHT]:
+        if keys[pygame.K_LEFT]:             # LEFT - Rotate camera left
             glRotatef(1, -1, 0, 0)
-        if keys[pygame.K_DOWN]:
-            glRotatef(1, 0, 1, 0)
-        if keys[pygame.K_UP]:
+        if keys[pygame.K_RIGHT]:            # RIGHT - Rotate camera right
+            glRotatef(1, 1, 0, 0)
+        if keys[pygame.K_DOWN]:             # DOWN - Rotate camera down
             glRotatef(1, 0, -1, 0)
-        if keys[pygame.K_PAGEUP]:
+        if keys[pygame.K_UP]:               # UP - Rotate camera up
+            glRotatef(1, 0, 1, 0)
+        if keys[pygame.K_PAGEUP]:           # PageUp - Zoom out
             glScalef(0.98, 0.98, 0.98)
-        if keys[pygame.K_PAGEDOWN]:
+        if keys[pygame.K_PAGEDOWN]:         # PageDown - Zoom in
             glScalef(1.02, 1.02, 1.02)
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_SPACE]:            # SPACE - Pause
             self.pause()
 
         # Get event queue and check it for a few specific events
@@ -125,14 +146,17 @@ class LightbotOpenGLWindow:
                                event.key == pygame.K_HOME)
 
             if event.type == pygame.QUIT or esc_is_pressed:
-                pygame.quit()
+                pygame.quit()               # ESC - quit the game
                 quit()
-            if home_is_pressed:
-                self.reset_camera_position()
+            if home_is_pressed:             # HOME - default camera position
+                self.set_default_camera_position()
+
             if log:
                 print(event)
 
-    def pause(self):
+    @staticmethod
+    def pause():
+        """A dummy function for the future."""
         pass
 
     @staticmethod
